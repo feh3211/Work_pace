1451bab869e79bc29c8ca00edae41a69f44ce0d1
Artorias3211 <felipenepomuceno2018@outlook.com>
2023-10-18 01:11:08 -0300

    entendendo variaveis

---
+++ JS/Aula02-Variaveis/exercicioAula02JS/main.js
@@ -0,0 +1,27 @@
+var nota1 = 7 
+
+var nota2 = 4
+
+var nota3 = 8
+
+var nota4 = 6
+
+var  mediaDasNotas = nota1 + nota2 + nota3 +nota4 
+
+console.log("A média do aluno com as 4 notas é:"+mediaDasNotas/4)
+
+/* RASCUNHO POR QUE INTERPRETEI O EXERCICIO DE DUAS FORMAS 
+
+var nota1 = 7
+
+var nota2 = 4
+
+var nota3 = 8
+
+var nota4 = 6
+
+var  mediaDasNotas = nota1 + nota2 + nota3 +nota4 
+
+alert("A média do aluno com as 4 notas é:"+mediaDasNotas)
+
+console.log(nota1 + nota2 + nota3 +nota4)*/
--- JS/Aula02-Variaveis/main.js
+++ JS/Aula02-Variaveis/main.js
@@ -2,11 +2,11 @@ var nomeDoPrimeiroAluno = "Felipe"
 
 console.log(nomeDoPrimeiroAluno)
 
+
 let numeroDeAprovados = 5 
 
 console.log(numeroDeAprovados)
 
-console.clear()
 
 let nomeAluno = "Felipe"
 
@@ -14,15 +14,15 @@ nomeAluno = "Victor"
 
 console.log(nomeAluno)
 
-//console.log (2 + 4)//
+console.log (2 + 4)
 
-/*var numero1 = 2
+var numero1 = 2
 
 var numero2 = 4
 
 var soma = numero1 + numero2;
 
-console.log(soma)*/
+console.log(soma)
 
 
 
@@ -34,8 +34,6 @@ var texto1 = "aelra lasldla lasdlasdl 1 \n"
 var texto2 = "asldjaskdhf asd asd \n asd as d2 222 2asd"
 
 console.log(texto1 + texto2)
-
-console.clear()
 
 var osDeVerdadeEuSeiQuemSao = true;
 
