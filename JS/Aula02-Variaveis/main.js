fd2ffaff5cb1d527399db441b87d28b635d61810
feh3211 <felipenepomuceno2018@gmail.com>
2023-10-17 21:52:10 -0300

    aula02 Variaveis

---
+++ JS/Aula02-Variaveis/main.js
@@ -0,0 +1,42 @@
+var nomeDoPrimeiroAluno = "Felipe"
+
+console.log("nomeDoPrimeiroAluno");
+
+let numeroDeAprovados = 5 
+
+console.log(numeroDeAprovados)
+
+console.clear()
+
+let nomeAluno = "Felipe"
+
+nomeAluno = "Victor"
+
+console.log(nomeAluno)
+
+//console.log (2 + 4)//
+
+/*var numero1 = 2
+
+var numero2 = 4
+
+var soma = numero1 + numero2;
+
+console.log(soma)*/
+
+
+
+
+//string//
+
+var texto1 = "aelra lasldla lasdlasdl 1 \n"
+
+var texto2 = "asldjaskdhf asd asd \n asd as d2 222 2asd"
+
+console.log(texto1 + texto2)
+
+console.clear()
+
+var osDeVerdadeEuSeiQuemSao = true;
+
+console.log("five" * 2) /*NaN*/
