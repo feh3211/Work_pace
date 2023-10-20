3ffb301e47c9c4a9e3b4dbb8533baf1369b913ba
Artorias3211 <felipenepomuceno2018@outlook.com>
2023-10-19 23:57:23 -0300

    exercicio casa string

---
+++ JS/Aula03-string/exercicio03/main.js
@@ -0,0 +1,3 @@
+const string01 = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+console.log(string01.charAt(19) + string01.length + string01.toUpperCase().substring(47))
--- JS/Aula03-string/main.js
+++ JS/Aula03-string/main.js
@@ -23,12 +23,12 @@ console.log("posição 10 é a letra: " + string01.charAt(10))
 const string02 = "instituto da oportunidade social"
 
 
-//.length
+//.length: a propriedade length retorna o tamanho da string, ou seja, o número de caracteres que a string possui.
 console.log("quantidade de caractere" + string02.length)
 console.clear()
 
 
-//toCase low e Upper
+//toCase low e Upper: Você pode transformar todos os caracteres de uma string para maiúsculo ou para minúsculo
 const string03 = "instituto da oportunidade social"
 
 const string04 = "INSTITUTO DA OPORTUNIDADE SOCIAL"
@@ -40,7 +40,7 @@ console.log("tudo em minusculo: " + string04.toLowerCase())
 console.clear()
 
 //--------------------------------------------------------------------
-//substring
+//substring: O método substring corta a string de acordo com os índices indicados entre os parênteses, por exemplo:
 var navegador= "MOZILLA"
 
 console.log(navegador.substring(3, 7));
@@ -48,7 +48,6 @@ console.log(navegador.substring(3, 7));
 console.log(navegador.substring(3));
 
 //replace: substituir uma string dentro de outra
-
 var trocaTexto = "Bem vindo ao jogo, roblox"
 
 var outraTroca = trocaTexto.replace("roblox", "Felipe");
@@ -59,7 +58,7 @@ console.log("texto alterado: " + outraTroca)
 console.clear()
 
 
-//trim:
+//trim: O método trim remove os espaços os espaços em branco do início de o final de uma string.
 var retirandoEspacos = "        A vida é uma uva       ";
 
 console.log(retirandoEspacos.trim())
