1c4e363495025d6b05a616fc7f03486ad9c27cd8
feh3211 <felipenepomuceno2018@gmail.com>
2023-10-24 21:53:01 -0300

    Aula04 condicionaisJS

---
+++ JS/Aula04-Condicionais/main.js
@@ -0,0 +1,73 @@
+let professoraBateuNoAluno = true
+let professoraXingouAluno = false 
+
+if(professoraBateuNoAluno == true && professoraXingouAluno == true){  
+    console.log("professora demitida com sucesso!!")
+}
+
+let numero1 = 10
+let numero2 = 4
+
+if(numero1 != numero2){
+    console.log("os dois são diferentes")
+}
+
+console.clear()
+
+let teraComidaTOTVS = true
+
+if(!teraComidaTOTVS == true){
+    console.log("Não vai almoçar por que eu sabo muito")
+}else{
+    console.log("Você terá que almoçar")
+}
+
+//________________________________________________________________
+
+let numero = 10 
+
+if(numero % 2 == 0){
+    console.log("O numero " + numero + " é par")
+}else{("O numero " + numero + " é impar")
+
+}
+//-----------------------------------------------------------------------
+
+let morangoMaduro = null //vazio
+
+if(morangoMaduro == true){
+    console.log("Pode comer")
+}else if (morangoMaduro == false){
+    console.log("não come")}
+else{ 
+    console.log("seu morango está mofado")
+}
+
+console.clear()
+//_____________________________________________________________________
+
+let caminhoDasIndias = 9
+let idade = 10
+
+if(caminhoDasIndias == 9){
+    console.log("Você está ficando velho");
+
+    if(idade == 27){
+        console.log("Cara, você está com o pé na cova")
+    }else{
+        console.log("Você ainda está novo")
+        }
+}
+//----------------------------------------------------------------------
+
+let fase2 = 70
+let fase3 = false
+let inscricao = true
+
+if(inscricao == true){
+    console.log("Parabéns você está na fase 2")
+}else if(fase2 >= 65 || fase2 <= 69){
+    console.log("Você terá uma segunda chance")
+}else{
+    console.log("Você não passou na fase2")
+}
