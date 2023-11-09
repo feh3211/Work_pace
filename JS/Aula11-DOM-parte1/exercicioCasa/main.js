fc0236085e0b090e00c2ce4073f7ebb0a68cd1e5
feh3211 <felipenepomuceno2018@gmail.com>
2023-11-08 21:23:31 -0300

    exercicio11

---
+++ JS/Aula11-DOM-parte1/exercicioCasa/main.js
@@ -0,0 +1,5 @@
+let h1 = document.createElement("h1")
+document.body.appendChild(h1)
+h1.innerHTML = "cadastro de usuario";
+
+document.title = "cadastro empresa Macron"
---
+++ JS/Aula11-DOM-parte1/main.js
@@ -0,0 +1,10 @@
+//criação da variavel e criação do botão
+let botao = document.createElement("BUTTON");
+//adicionei o filho na body
+document.body.appendChild(botao)
+//adicionei um texto dentro do botao
+botao.innerHTML = "acessa ai";
+//adicionar estilo ao botao
+botao.style.bordertop = "black"
+document.body.style.backgroundColor = "greenyellow"
+botao.style.border = "solid 2px blue";
